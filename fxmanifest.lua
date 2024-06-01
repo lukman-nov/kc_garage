77d12edce7e9773a545a34c9544ae8e2bbfe3f07
lukman-nov <novawiyadi@gmail.com>
2024-06-01 23:22:43 +0700

    bump version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,10 @@ fx_version 		'adamant'
 game 					'gta5'
 lua54 				'yes'
 
-name 					'Kucluck Garage V2'
-description 	'Advanced Garages for ESX & QB V2'
+name 					'kc_garage'
+description 	'Advanced Garages for ESX & QB'
 author 				'Lukman_Nov#5797'
-version 			'2.5.1'
+version 			'2.5.2'
 license    		'GNU General Public License v3.0'
 
 shared_scripts {
@@ -14,7 +14,8 @@ shared_scripts {
   'shared/config.lua',
   'shared/garages.lua',
   'shared/impounds.lua',
-  'locales/*.lua'
+  'locales/*.lua',
+	'function.lua'
 }
 
 client_scripts {
@@ -33,24 +34,13 @@ server_scripts {
 	'server/update.lua',
 }
 
-ui_page "web/index.html"
-
-files {
-	"web/assets/*",
-	"web/assets/**/*",
-	"web/index.html",
-	"web/script.js",
-	"web/style.css",
-}
-
 escrow_ignore {
 	'bridge/**/*.lua',
-	'client/main.lua',
-	'client/function.lua',
-	'client/mainAPI.lua',
-	'client/vehicle_names.lua',
+	'client/*.lua',
 	'locales/*.lua',
 	'server/main.lua',
 	'shared/*.lua',
 	'function.lua',
 }
+
+dependency '/assetpacks'
